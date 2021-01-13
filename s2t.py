9a5c9942e98f10a48046c76438aeba2a41d4dbe7
Tricky4 <64266145+Tricky4@users.noreply.github.com>
2021-01-13 08:31:28 +0530

    Create s2t.py

---
+++ s2t.py
@@ -0,0 +1,9 @@
+import speech_recognition as sr
+r = sr.Recognizer()
+
+print("Say Something...")
+with sr.Microphone() as source:
+    audio_data = r.record(source, duration=5)
+    print("Recognizing...")
+    text = r.recognize_google(audio_data)
+    print(text)
